ec78e78947a9465a45e06699121235e8d1a27b0c
Terpsipe <annerose.tartler@gmail.com>
2024-02-13 23:17:30 +0100

    book ready for backend

--- js/book.js
+++ js/book.js
@@ -1,11 +1,21 @@
-const sendButton = document.querySelector('button[type="submit"]');
+document.addEventListener('DOMContentLoaded', function() {
+    const sendButton = document.querySelector('button[type="submit"]');
 
-sendButton.addEventListener('click', function(event) {
-    event.preventDefault();
-    findTable();
+    sendButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        findTable();
+    });
+
+    // This script captures form data (name, date, time) and currently logs it to the console.
+    // In the future, a backend component will handle this data, allowing for full functionality like looking for tables and reserving them.
+    // The current state is a temporary solution and I'm planing on coming back to it when implementing a real backend
+    function findTable(){
+        const name = document.getElementById('name').value;
+        const date = document.getElementById('date').value;
+        const time = document.getElementById('time').value;
+
+        console.log("Name:", name);
+        console.log("Date:", date);
+        console.log("Time:", time);
+    };
 });
-
-// This script is currently empty and will be revisited when I implement the backend
-function findTable(){
-
-};
