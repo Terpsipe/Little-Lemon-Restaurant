66b3927be9bc9599ba3b27591549113d12521318
Terpsipe <annerose.tartler@gmail.com>
2024-02-13 23:10:39 +0100

    book waiting for backend

---
+++ js/book.js
@@ -0,0 +1,11 @@
+const sendButton = document.querySelector('button[type="submit"]');
+
+sendButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    findTable();
+});
+
+// This script is currently empty and will be revisited when I implement the backend
+function findTable(){
+
+};
