41fb60e7901044b8ad1e4e5ae4de8a3b98e949ed
Terpsipe <annerose.tartler@gmail.com>
2024-02-13 22:58:53 +0100

    contact awaiting backend work

---
+++ js/contact.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('yep');
+
+    const sendButton = document.querySelector('button[type="submit"]');
+
+    sendButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        sendMessage();
+    });
+
+    // This script captures form data (name, email, and message) and currently logs it to the console.
+    // In the future, a backend component will handle this data, allowing for full functionality like sending emails.
+    // This is a temporary solution and I'm planing on coming back to it when implementing a real backend
+    function sendMessage() {
+        const name = document.getElementById('name').value;
+        const mail = document.getElementById('mail').value;
+        const message = document.getElementById('message').value;
+
+        console.log("Name:", name);
+        console.log("Email:", mail);
+        console.log("Message:", message);
+    }
+});
